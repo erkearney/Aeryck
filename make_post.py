542208a26b3cb5377fdbfba5461809d029330c4f
e <erickearney@protonmail.com>
2023-04-13 08:51:03 -0600

    Update: added syntax highlighting with pygments

--- make_post.py
+++ make_post.py
@@ -1,10 +1,14 @@
-import os
 import argparse
 import sqlite3
+from pathlib import Path
 from sqlite3 import Error
 
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import HtmlFormatter
 
-def get_filename():
+
+def parse_args():
     parser = argparse.ArgumentParser(description='Ingest a markdown file and'
                                             ' insert it into the database')
     parser.add_argument('FILENAME', help='Path to the markdown file to be'
@@ -13,11 +17,9 @@ def get_filename():
                         help='If enabled, update an existing post instead of '
                         'creating a new one')
     args = parser.parse_args()
-    filename = args.FILENAME
-    global UPDATE
-    UPDATE = args.update
+    filepath = Path(args.FILENAME)
 
-    return filename
+    return filepath, args.update
 
 
 def create_connection(db_file) -> 'sqlite3.connect':
@@ -30,27 +32,34 @@ def create_connection(db_file) -> 'sqlite3.connect':
     return database
 
 
-def read_post(filename) -> 'String':
-    with open(filename) as f:
+def read_post(path) -> 'String':
+    with path.open() as f:
         text = f.readlines()
 
     return text
 
 
-def ingest_file(filename) -> 'Tuple':
-    title = str(os.path.basename(filename)).removesuffix('.md').replace('_', ' ')
-    text = read_post(filename)
+def ingest_file(path) -> 'Tuple':
+    title = path.stem.removesuffix('.md').replace('_', ' ')
+    text = read_post(path)
     body = ''
+    code_block = ''
     for line in text:
-        body += line
+        if line.startswith(' ' * 4):
+            code_block += line[4:]
+        elif code_block:
+            body += highlight(code_block, PythonLexer(), HtmlFormatter())
+            code_block = ''
+        else:
+            body += line
 
     return (body, title)
 
 
-def insert_into_db(database, post):
+def insert_into_db(database, post, update=False):
     cursor = database.cursor()
 
-    if UPDATE:
+    if update:
         sql = ''' UPDATE post SET body = (?) WHERE title = (?) '''
     else:
         sql = ''' INSERT INTO post(body, title) VALUES(?,?) '''
@@ -59,8 +68,9 @@ def insert_into_db(database, post):
 
 
 if __name__ == '__main__':
-    filename = get_filename()
+    filepath, update = parse_args()
     database_file = r"instance/aeryck.sqlite"
     database = create_connection(database_file)
-    ingested_file = ingest_file(filename)
-    insert_into_db(database, ingested_file)
+
+    ingested_file = ingest_file(filepath)
+    insert_into_db(database, ingested_file, update=update)
